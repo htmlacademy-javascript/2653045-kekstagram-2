24c3b48ecc70be8c092cdb317c2ec04898735c35
Kostiantyn Drozd <drozd47520@gmail.com>
2025-12-04 02:06:06 +0000

    Fix: Import thumbnails.js module to enable click handlers

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { generatePhotoDescriptions } from './data.js';
 import { initForm } from './form.js';
+import './thumbnails.js'; // Импорт модуля для отрисовки миниатюр и обработчиков кликов
 
 const photoDescriptions = generatePhotoDescriptions();
 // eslint-disable-next-line no-console
